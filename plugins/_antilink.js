9ec744ec7f87bd179e4606b2971e0990eedce3e5
XX88IP <xx88ip@gmail.com>
2026-05-13 22:27:45 +0100

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,58 +1,47 @@
-const isLinkHttp = /https:\/\/chat\.whatsapp\.com\/[A-Za-z0-9]{22}/i
-const isWhatsAppChannel = /https:\/\/whatsapp\.com\/channel\/[A-Za-z0-9?=._-]+/i
+// @instagram: sunoovvv
+const isLinkHttp = /https:\/\/chat\.whatsapp\.com\/[A-Za-z0-9]{22}/i;
+const isWhatsAppChannel = /https:\/\/whatsapp\.com\/channel\/[A-Za-z0-9?=._-]+/i;
 
 export async function before(
   m,
-  { conn, isAdmin, isBotAdmin },
+  { conn, args, usedPrefix, command, isAdmin, isBotAdmin },
 ) {
-  if (m.isBaileys && m.fromMe) return !0
-  if (!m.isGroup) return !1
+  if (m.isBaileys && m.fromMe) return !0;
+  if (!m.isGroup) return !1;
 
-  let chat = global.db.data.chats[m.chat]
+  let chat = global.db.data.chats[m.chat];
+  let name = conn.getName(m.sender);
 
-  const isGroupLink = isLinkHttp.test(m.text)
-  const isChannelLink = isWhatsAppChannel.test(m.text)
+  const isGroupLink = isLinkHttp.test(m.text);
+  const isChannelLink = isWhatsAppChannel.test(m.text);
 
   function cek(grup1, grup2) {
-    const regex = /^https:\/\/chat\.whatsapp\.com\/[a-zA-Z0-9]{22}$/
-    return regex.test(grup1) && regex.test(grup2) && grup1 === grup2
+    const regex = /^https:\/\/chat\.whatsapp\.com\/[a-zA-Z0-9]{22}$/;
+    return regex.test(grup1) && regex.test(grup2) && grup1 === grup2;
   }
 
   if (chat.antiLink && (isGroupLink || isChannelLink)) {
-
-    let grup = await conn.groupInviteCode(m.chat)
-
-    let gc = await cek(
-      m.text,
-      "https://chat.whatsapp.com/" + grup
-    )
+    let grup = await conn.groupInviteCode(m.chat);
+    let gc = await cek(m.text, "https://chat.whatsapp.com/" + grup);
 
     await m.reply(
       gc
-        ? "You sent this group link. You are safe!"
+        ? "لقد أرسلت رابط المجموعة هذا. أنت آمن!"
         : !isAdmin
-          ? "*❗ Group or channel link detected*\n*Your message will be deleted and you will be removed from the group*"
-          : "*📣 You are an admin, you are safe*",
-    )
+          ? "*❗ تم اكتشاف أنك ترسل رابط مجموعة أو قناة*\n*سيتم حذف رسالتك من قبل البوت لأنه ليس لديك صلاحية*"
+          : "*📣 أنت مسؤول، أنت آمن*",
+    );
 
-    await conn.delay(1000)
+    await conn.delay(1000);
 
     if (!gc && isBotAdmin && !isAdmin) {
+      await conn.sendMessage(m.chat, {
+        delete: m.key,
+      });
 
-      await conn.sendMessage(
-        m.chat,
-        {
-          delete: m.key,
-        }
-      )
-
-      await conn.groupParticipantsUpdate(
-        m.chat,
-        [m.sender],
-        'remove'
-      )
+      // لإزالة العضو، يمكنك تفعيل هذا السطر:
+      // await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
     }
   }
-
-  return !1
+  return !1;
 }
